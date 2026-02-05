bb1e50e219493c82a19da55d6e440a7a5fbeb904
Goulin Khoge <36013798+goulinkh@users.noreply.github.com>
2026-02-05 20:21:45 +0100

    feat(login): implement login flow for GitHub, Launchpad & OIDC

--- src/lib/api/cla/client.ts
+++ src/lib/api/cla/client.ts
@@ -3,7 +3,7 @@ import createClient from 'openapi-fetch';
 import type { paths } from './types.js';
 
 export const claApi = createClient<paths>({
-	baseUrl: env.CLA_API_URL,
+	baseUrl: env.ORIGIN,
 	headers: {
 		'X-Internal-Secret': env.CLA_INTERNAL_API_SECRET
 	}
---
+++ src/lib/cookies.ts
@@ -0,0 +1,3 @@
+export const OIDC_COOKIE = 'canonical_oidc_session' as const;
+export const GITHUB_COOKIE = 'github_access_token_session' as const;
+export const LAUNCHPAD_COOKIE = 'launchpad_access_token_session' as const;
